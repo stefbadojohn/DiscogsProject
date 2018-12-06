6879719cef1e1c3bd823c7881e31ea5497e426d4
StefBadojohn <stefbadojohn@gmail.com>
2018-12-06 12:21:00 +0200

    Added artist class

---
+++ app/src/main/java/com/example/stefbadojohn/discogsproject/DiscogsArtist.java
@@ -0,0 +1,14 @@
+package com.example.stefbadojohn.discogsproject;
+
+public class DiscogsArtist {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
